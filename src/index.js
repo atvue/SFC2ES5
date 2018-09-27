85305fdf5b244a31ae0786a2fc8d3c8afd8ca2a2
毛庭峰 <hzmaotingfeng@corp.netease.com>
2018-09-27 23:20:36 +0800

    添加入口文件,入口文件分发组件

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// this file is not used if use https://www.npmjs.com/package/babel-plugin-import
+var ENV = process.env.NODE_ENV;
+if (ENV !== 'production' && ENV !== 'test' && typeof console !== 'undefined' && console.warn && typeof window !== 'undefined') {
+    console.warn('You are using a whole package of bui, ' + 'please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.');
+}
+
+export { default as Alert } from './alert'
