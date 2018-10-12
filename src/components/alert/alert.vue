05138076772a915ca9c8c1a8c2c0c746e458b442
毛庭峰 <hzmaotingfeng@corp.netease.com>
2018-10-12 20:44:31 +0800

    mock 组件相互引用传递数据

--- src/components/alert/alert.vue
+++ src/components/alert/alert.vue
@@ -1,17 +1,23 @@
 <template>
     <div>
         alert,{{name}}
+        <test name="maotingfeng">
+            哈哈哈哈
+        </test>
     </div>
 </template>
 
 
 <script>
 import { abc } from './util.js'
-
+import test from './test'
 abc()
 
 export default {
     name: 'alert'  ,
+    components: {
+        test
+    } ,
     data(){
         return {
             name: "毛庭峰"
---
+++ src/components/alert/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        this is test,{{name}}
+        <slot></slot>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: [ 'name' ]
+}
+</script>
+
